b3cacc26fb052978579bd377a33cd28f8aeab586
admin <buygameraidee@gmail.com>
2019-07-09 15:27:24 +0700

    Edit Table

--- application/views/view.php
+++ application/views/view.php
@@ -30,10 +30,6 @@
             </tr>
         </thead>
         <tbody class="list">
-            <?php
-            if(isset($view_data) && is_array($view_data) && count($view_data)): $i=1;
-            foreach ($view_data as $key => $data) { 
-            ?>
 
             <tr>
                 <th scope="row" class="S.No">
@@ -60,7 +56,7 @@
                     </div>
                     
                 </td>
-                <?php } endif; ?>
+              
             </tr>
             </tbody>
     </table>
@@ -68,7 +64,91 @@
                 
                 <button type="submit" class="btn btn-success btn-lg" style="margin-top: 44px; margin-bottom: 44px; width:120px;" >ยืนยัน</button>
                 
-                
+                <div class="col">
+          <div class="card shadow">
+            <div class="card-header border-0">
+              <h3 class="mb-0">Card tables</h3>
+            </div>
+            <div class="table-responsive">
+              <table class="table align-items-center table-flush">
+                <thead class="thead-light">
+                  <tr>
+                    <th scope="col">S.No</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Class</th>
+                    <th scope="col">Manage</th>
+                        
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                <?php
+                    if(isset($view_data) && is_array($view_data) && count($view_data)): $i=1;
+                    foreach ($view_data as $key => $data) { 
+                    ?>
+                  <tr>
+                    <th scope="row">
+                      <div class="media align-items-center">
+                        <a href="#" class="avatar rounded-circle mr-3">
+                          <img alt="Image placeholder" src="../assets/img/theme/bootstrap.jpg">
+                        </a>
+                        <div class="media-body">
+                          <span class="mb-0 text-sm">ชื่อไฟล์ที่อัพ</span>
+                        </div>
+                      </div>
+                    </th>
+                    <td>
+                    โดย ชื่อผู้อัพ
+                    </td>
+                    <td>
+                      <span class="badge badge-dot mr-4">
+                        <i class="bg-warning"></i> pending
+                      </span>
+                    </td>   
+                    <td class="text-right">
+                      <div class="dropdown">
+                        <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <i class="fas fa-ellipsis-v"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow" x-placement="top-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(32px, -2px, 0px);">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <?php } endif; ?>
+                </tbody>
+              </table>
+            </div>
+            <div class="card-footer py-4">
+              <nav aria-label="...">
+                <ul class="pagination justify-content-end mb-0">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">
+                      <i class="fas fa-angle-left"></i>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                  </li>
+                  <li class="page-item active">
+                    <a class="page-link" href="#">1</a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">
+                      <i class="fas fa-angle-right"></i>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
             </form>
             </div>
   </div>
