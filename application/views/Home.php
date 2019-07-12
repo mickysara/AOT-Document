ad61a64ecf24e82a7b186669f4f5db46b31d2cb2
admin <buygameraidee@gmail.com>
2019-07-12 10:29:32 +0700

    EditHome

--- application/views/Home.php
+++ application/views/Home.php
@@ -22,11 +22,11 @@
   <div class="row">
   <?php for( $i = 0; $i < 6; $i++) 
     { ?>
-    <div class="col-sm">
+    <div class="col-sm" style="margin-right: auto; margin-left: auto;">
 
 
     
-    <div class="card" style="width: 18rem; margin-top: 20px; margin-bottom: 20px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+    <div class="card" style="width: 18rem; margin-top: 20px; margin-bottom: 20px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); ">
             <img class="card-img-top" src="../assets/img/card/powerpoint.png" alt="Card image cap">
             <div class="card-body">
                 <h3 class="card-title">ชื่อ : </h3>
@@ -35,8 +35,42 @@
                 <a href="#" class="btn btn-primary" style="background-color:#2d3436;">ดูรายละเอียดเพิ่มเติม</a>
             </div>
         </div>
-     
- </div>
+    </div>
  <?php } ?>
   </div>
+          <button type="button" class="btn btn-primary btn-lg btn-block mb-5 mt-5" style="width: 1056px; font-size: 20px; background-color: #2d3436;">ดูเอกสารทั้งหมด</button>
+          <div class="container page-styling row-wrap-container row-wrap-nottl front-icons" style="width: 1056px; margin-bottom: 40px;">
+                <div class="row">
+                <div class="cf-lg-4 col-sm-4">
+                  <div class="iconbox-item iconbox-i">
+                    <div class="iconbox-i-top">
+                      <p class="iconbox-i-img withimg">
+                      </p>
+                    <h1 style="font-size: 45px; text-align: center; color:#2d3436;">ค้นหา!</h1>
+                    </div>
+                    <p style="font-size: 18px;">ค้นหาเอกสาร หรือ ไฟล์ที่ต้องการจะดาวน์โหลดที่อยู่ในคลัง</p>
+                  </div>
+                </div>
+                <div class="cf-lg-4 col-sm-4">
+                  <div class="iconbox-item iconbox-i">
+                    <div class="iconbox-i-top">
+                        <p class="iconbox-i-img withimg">
+                        </p>
+                        <h1 style="font-size: 45px; text-align: center; color:#2d3436;">แสกน!</h1>
+                      </div>
+                      <p style="font-size: 18px; ">ด้วยช่องทางการชำระเงินแบบใหม่ ท่านไม่จำเป็นต้องเติมเงินอีกต่อไป เพียงกดสั่งซื้อระบบจะสร้างบิลให้ท่านชำระตามบิลได้ทันที เมื่อชำระเสร็จระบบจะทำรายการต่อให้เลย</p>
+                  </div>
+                </div>
+                <div class="cf-lg-4 col-sm-4">
+                  <div class="iconbox-item iconbox-i">
+                    <div class="iconbox-i-top">
+                      <p class="iconbox-i-img withimg">
+                      </p>
+                      <h1 style="font-size: 45px;  text-align: center; color:#2d3436;">ดาวน์โหลด</h1>
+                    </div>
+                    <p style="font-size: 18px;">ไข่หอมจัดส่งด้วยระบบอัตโนมัติทั้งแต่ต้นจนจบ ท่านสามารถทำทุกขั้นตอนด้วยตัวเองได้ตั้งแต่แรกจนรับสินค้าโดยไม่ต้องรอแอดมิน ไม่ว่าจะสั่งดึกแค่ไหนก็มีบอทคอยส่ง!</p>
+                    </div>
+                  </div>
+                </div>
+                </div>
 </div>
