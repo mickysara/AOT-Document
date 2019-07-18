f595b559429049c497f8ddc1779d53438deb9c87
admin <buygameraidee@gmail.com>
2019-07-18 15:35:21 +0700

    OK

--- application/views/Footer.php
+++ application/views/Footer.php
@@ -135,6 +135,21 @@ $(document).ready( function () {
             });
           });
 </script>
+<script>
+$(document).ready(function(e) {
+	increaseNotify();
+    setInterval(increaseNotify, 3000);
+});
+function increaseNotify(){ // โหลดตัวเลขทั้งหมดที่ถูกส่งมาแสดง
+          $.get("<?=base_url('index.php/LoginController/IncreaseNoti')?>", 
+              function (data) {
+                  
+                $("#Noti").html(data)
+
+              }
+          );
+}
+</script>
 
 
 <!-- Syntax Highlighter -->
