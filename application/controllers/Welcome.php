7234d4e8eb19f7fa62f5f5308e05af2be5789dbe
Harmonic <buygameraidee@gmail.com>
2019-08-15 14:56:33 +0700

    ok

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,9 +20,6 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		// $this->load->view('Header');
 		$this->load->view('welcome_message');
-		// $this->load->view('Footer');
 	}
-	
 }
