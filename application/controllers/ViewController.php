71b5a645fdccef85b7706fed4cc99343fa234bcd
PooMPF1 <poom_darknes1@hotmail.com>
2019-09-16 12:46:45 +0700

    test

--- application/controllers/ViewController.php
+++ application/controllers/ViewController.php
@@ -13,7 +13,7 @@ class ViewController extends CI_Controller {
       if($this->session->userdata('_success') == '')
       {
           $this->load->view('Header');
-          $this->load->view('Loginalert');     
+          $this->load->view('LoginAlert');     
           $this->load->view('Footer');
       }else{
         redirect('ViewController/checkstatus');
