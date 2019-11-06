3764aa1868086e35726c96b0e7690a00d0bbd354
HARMONIC <mickysara@users.noreply.github.com>
2019-11-06 23:48:33 +0700

    Update LoginController.php

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -26,67 +26,18 @@ class LoginController extends CI_Controller {
       $username =  $this->input->post("username");
       $password =  $this->input->post("password");
 
-      $curl = curl_init();
-
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.airportthai.co.th/v2/Authen/AuthenUser/",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => "username=" . $username . "&password=" . $password,
-        //CURLOPT_POSTFIELDS => "username=".$username."&password=".$password,
-        CURLOPT_HTTPHEADER => array(
-          "Postman-Token: c3c8b2b9-146d-4a89-b633-be954b49d589",
-          "cache-control: no-cache",
-          "x-api-key: 0HyLnGPCibLKetyPyzZJzhfI32d3V3kC2e8FkSt4"
-        ),
-      ));
-
-      $response = curl_exec($curl);
-      $err = curl_error($curl);
+      $this->db->where('Id_Emp', $username);
+      $this->db->where('Password', $password);
+      $query = $this->db->get('Users', 1);
       
-      curl_close($curl);
-      
-      if ($err) {
-        echo "cURL Error #:" . $err;
-      } else {
-
-         $data = json_decode($response, 1);
         
          
-         if($data['_success'] == 1)
+         if($query->num_rows() == 1)
          {
-           
-                $this->db->where('Id_Emp', $data['employeeId']);
-          $query = $this->db->get('Users', 1);
-          $result = $query->num_rows();
-          
-          if($result == 0)
-          {
-            $dbinsert = array(
-              'Id_Emp'  =>  $data['employeeId'],
-              'Fname'  =>  $data['firstName'],
-              'Lname'  =>  $data['lastName'],
-              'AccName'  =>  $data['accountName'],
-              'Status'      =>  'user'
-            );
-
-            $this->db->insert('Users', $dbinsert);
-            $data['Status'] = $d['Users'];
-            
-          }else{
-
-            $d = $query->row_array();
-
-            $data['Status'] = $d['Status'];
-
-          }
-          
-
-           
+           $data = $query->row_array();
+           $data['_success'] = 1;
+           $data['employeeId'] = $data['Id_Emp'];
+           $data['accountName'] = $data['AccName'];
            $this->session->set_userdata($data);
 
            if($this->session->userdata('login_referrer')!=""){
@@ -118,7 +69,7 @@ class LoginController extends CI_Controller {
            $ip = explode(',',$ipaddress);
            
            $object = array(
-             'Id_Emp' =>  $data['employeeId'],
+             'Id_Emp' =>  $data['Id_Emp'],
              'Ip'     =>  $ip[0],
              'Action' =>  'เข้าสู่ระบบ'
            );
@@ -133,7 +84,7 @@ class LoginController extends CI_Controller {
       
           
          }
-      }
+      
     }
 
     public function Logout()
